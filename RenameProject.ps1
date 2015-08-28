650f82fa51b86fa8e1682a4784ea2ee24ef7326d
djturner1984@gmail.com <atitagain>
2015-08-29 08:50:49 +1000

    added project name input parameter

--- RenameProject.ps1
+++ RenameProject.ps1
@@ -1,9 +1,15 @@
-Get-ChildItem -filter "*Project.Name*" -Recurse | Rename-Item -NewName { $_.Name -replace "Project.Name","New.Website" }
+$name = Read-Host 'What is your new project namespace?'
 
+If ($name -Match " ")
+{
+    "No spaces allowed"
+    exit
+}
 
+Get-ChildItem -filter "*Project.Name*" -Recurse | Rename-Item -NewName { $_.Name -replace "Project.Name", $name }
 $files=get-childitem . *.* -rec | where { ! $_.PSIsContainer }
 foreach ($file in $files)
 {
-    (Get-Content $file.PSPath) | Foreach-Object {$_ -replace "Project.Name", "New.Website"} | Set-Content $file.PSPath
+    (Get-Content $file.PSPath) | Foreach-Object {$_ -replace "Project.Name", $name} | Set-Content $file.PSPath
 }
 
